658b5e8b589d7b9e69908982864958086e593579
Priyal parmar <parmarpriyal1603@gmail.com>
2026-09-03 22:33:27 +0530

    Update compas_demo.py

--- examples/compas_demo.py
+++ examples/compas_demo.py
@@ -4,12 +4,10 @@ from fairlens.audit import FairnessAuditor
 df = load_compas_dataset()
 
 auditor = FairnessAuditor()
-
 result = auditor.audit(
     dataframe=df,
     target_column="two_year_recid",
     prediction_column="prediction",
     sensitive_feature="race",
 )
-
 print(result)
