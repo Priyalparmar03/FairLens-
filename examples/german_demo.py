3b6141245a3bb1355ef715f90cae9198ad598ee9
Priyal parmar <parmarpriyal1603@gmail.com>
2026-09-03 22:33:13 +0530

    Update german_demo.py

--- examples/german_demo.py
+++ examples/german_demo.py
@@ -4,12 +4,10 @@ from fairlens.audit import FairnessAuditor
 df = load_german_dataset()
 
 auditor = FairnessAuditor()
-
 result = auditor.audit(
     dataframe=df,
     target_column="Target",
     prediction_column="prediction",
     sensitive_feature="Age",
 )
-
 print(result)
