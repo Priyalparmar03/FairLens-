db94ea37dd28a17be58a5338045cb08a87a7d8fa
Priyal parmar <parmarpriyal1603@gmail.com>
2026-09-03 22:33:02 +0530

    Update synthetic_demo.py

--- examples/synthetic_demo.py
+++ examples/synthetic_demo.py
@@ -4,12 +4,10 @@ from fairlens.audit import FairnessAuditor
 df = generate_synthetic_dataset()
 
 auditor = FairnessAuditor()
-
 result = auditor.audit(
     dataframe=df,
     target_column="Target",
     prediction_column="prediction",
     sensitive_feature="Gender",
 )
-
 print(result)
