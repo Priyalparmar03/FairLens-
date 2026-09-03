a63fd0fc69474947e27ada1b2ed64a5d26ae7bf4
Priyal parmar <parmarpriyal1603@gmail.com>
2026-09-03 22:32:50 +0530

    Update run_demo.py

--- examples/run_demo.py
+++ examples/run_demo.py
@@ -8,15 +8,5 @@ datasets = [
 for dataset in datasets:
 
     print("=" * 60)
-
     print(dataset)
-
     print("=" * 60)
-
-    # Load dataset
-
-    # Run FairLens
-
-    # Generate report
-
-    # Print summary
